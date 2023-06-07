408119623be4e8212db45e90d64ba1e1eb8a1825
mdikavw <mdikavw@gmail.com>
2023-06-07 13:48:43 +0700

    add nota.txt and idPemesanan

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <stdlib.h>
+#include <random>
 
 class Item{
     public:
@@ -40,6 +41,7 @@ class Menu{
             daftarMenu.push_back(newItem);
             id++;
         }
+        menuFile.close();
     }
     
     void printMenu(){
@@ -52,7 +54,6 @@ class Menu{
 class Pelanggan{
     public:
         int idPelanggan;
-        int total = 0;
         std::vector<Item> pesanan;
     Pelanggan(int x):idPelanggan(x){};
     
@@ -65,7 +66,6 @@ class Pelanggan{
             std::cout << "Pilih pesanan untuk pelanggan " << idPelanggan << ": " << std::endl;
             std::cin >> idItem;
             pesanan.push_back(menu.daftarMenu[idItem - 1]);
-            total = total + menu.daftarMenu[idItem - 1].harga;
             std::cout << "Apakah pelanggan " << idPelanggan << " ingin menambah pesanan?(y/n): ";
             std::cin >> pilihan;
         } while(pilihan == 'y');
@@ -73,15 +73,17 @@ class Pelanggan{
 };
 
 class Nota{
-    private:
-        std::vector<Item> daftarPesanan;
     public:
-        int id;
-        int total;
+        int idNota;
+        int idPemesanan;
+        int total = 0;
+        std::vector<Item> daftarPesanan;
         std::vector<int> jumlahPesanan;
-    Nota(int x, int y):id(x), total(y){}
+    Nota(int x, int y):idNota(x), idPemesanan(y){}
 
     void printNota(){
+        std::ofstream fileNota;
+        fileNota.open("nota.txt", std::fstream::app);
         for(int i = 0; i < daftarPesanan.size() - 1; i++){
             for(int j = i + 1; j < daftarPesanan.size(); j++){
                 if(daftarPesanan[i].idItem == daftarPesanan[j].idItem){
@@ -93,9 +95,12 @@ class Nota{
             }
         }
         for(int i = 0; i < daftarPesanan.size(); i++){
+            fileNota << daftarPesanan[i].nama << std::endl;
             std::cout << daftarPesanan[i].nama << std::endl;
+            fileNota << "x" << jumlahPesanan[i] << "\t\t\t" << daftarPesanan[i].harga*jumlahPesanan[i]<< std::endl << std::endl;
             std::cout << "x" << jumlahPesanan[i] << "\t\t\t" << daftarPesanan[i].harga*jumlahPesanan[i]<< std::endl << std::endl;
         }
+        fileNota << std::endl << "Total:\t\t\t" << total << std::endl;
         std::cout << std::endl << "Total:\t\t\t" << total << std::endl;
     }
     void generateDaftarPesanan(std::vector<Pelanggan> pelanggan){
@@ -110,20 +115,34 @@ class Nota{
     }
 
     void printHeader(int x){
+        std::ofstream fileNota;
+        fileNota.open("nota.txt", std::fstream::app);
+        fileNota << "----------Nota----------" << std::endl;
         std::cout << "----------Nota----------" << std::endl;
-        std::cout << "Id Meja: " << id << std::endl;
+        fileNota << "Id Pemesanan: " << idPemesanan << std::endl;
+        std::cout << "Id Pemesanan: " << idPemesanan << std::endl;
+        fileNota << "Id Meja: " << idNota << std::endl;
+        std::cout << "Id Meja: " << idNota << std::endl;
+        fileNota << "Jumlah pelanggan: " << x << std::endl;
         std::cout << "Jumlah pelanggan: " << x << std::endl;
+        fileNota << std::endl << std::endl;        
         std::cout << std::endl << std::endl;        
+    }
+
+    void hitungTotal(std::vector<Pelanggan> pelanggan){
+        for(int i = 0; i < pelanggan.size(); i++){
+            for(int j = 0; j < pelanggan[i].pesanan.size(); j++){
+                total = total + pelanggan[i].pesanan[j].harga;
+            }
+        }
     }
 };
 
 class NotaIndividu : public Nota{
     public:
-    int id;
-    int total;
+    int idNotaIndividu;
     NotaIndividu(int x, int y):Nota(x, y){
-        id = x;
-        total = y;
+        idNotaIndividu = x;
     }
     void generateJumlahPesanan(std::vector<Item> pesanan){
         for(int i = 0; i < pesanan.size(); i++){
@@ -131,12 +150,23 @@ class NotaIndividu : public Nota{
         }
     }
     void printHeader(int x, int y){
+        std::ofstream fileNota;
+        fileNota.open("nota.txt", std::fstream::app); 
+        fileNota << "----------Nota----------" << std::endl;
         std::cout << "----------Nota----------" << std::endl;
+        fileNota << "Id Pemesanan: " << idPemesanan << std::endl;
+        std::cout << "Id Pemesanan: " << idPemesanan << std::endl;
+        fileNota << "Id Meja: " << x << std::endl;
         std::cout << "Id Meja: " << x << std::endl;
+        fileNota << "Id Pelanggan: " << y << std::endl;
         std::cout << "Id Pelanggan: " << y << std::endl;
-        std::cout << std::endl << std::endl;        
+        fileNota << std::endl << std::endl;        
+        std::cout << std::endl << std::endl;
+        fileNota.close();        
     }
     void printNota(std::vector<Item> pesanan){
+        std::ofstream fileNota;
+        fileNota.open("nota.txt", std::fstream::app);
         for(int i = 0; i < pesanan.size() - 1; i++){
             for(int j = i + 1; j < pesanan.size(); j++){
                 if(pesanan[i].idItem == pesanan[j].idItem){
@@ -148,25 +178,34 @@ class NotaIndividu : public Nota{
             }
         }
         for(int i = 0; i < pesanan.size(); i++){
+            fileNota << pesanan[i].nama << std::endl;
             std::cout << pesanan[i].nama << std::endl;
+            fileNota << "x" << jumlahPesanan[i] << "\t\t\t" << pesanan[i].harga*jumlahPesanan[i]<< std::endl << std::endl;
             std::cout << "x" << jumlahPesanan[i] << "\t\t\t" << pesanan[i].harga*jumlahPesanan[i]<< std::endl << std::endl;
         }
-        std::cout << std::endl << "Total:\t\t\t" << total << std::endl;
+        fileNota << std::endl << "Total:\t\t\t" << total << std::endl << std::endl;
+        std::cout << std::endl << "Total:\t\t\t" << total << std::endl << std::endl;
+        fileNota.close();
+    }
+
+    void hitungTotal(std::vector<Item> pesanan){
+        for(int i = 0; i < pesanan.size(); i++){
+            total = total + pesanan[i].harga;
+        }
     }
 };
 
 class Meja{
     public:
         int jumlahPelanggan;
-        int id = 1;
-        int total = 0;
+        int idMeja;
+        int idPemesanan;
         std::vector<Pelanggan> pelanggan;
 
-    Meja(int x, int y, Menu menu):id(x), jumlahPelanggan(y){
+    Meja(int x, int y, int z ,Menu menu):idMeja(x), jumlahPelanggan(y), idPemesanan(z){
         for(int i = 0; i < jumlahPelanggan; i++){
             tambahPelanggan(i + 1, menu);
         }
-        total = hitungTotal();
     }
 
     void tambahPelanggan(int id, Menu menu){
@@ -175,23 +214,18 @@ class Meja{
         pelanggan.push_back(newPelanggan);
     }
 
-    int hitungTotal(){
-        for(int i = 0; i < pelanggan.size(); i++){
-            total = total + pelanggan[i].total;
-        }
-        return total;
-    }
-
     ~Meja(){
-        Nota nota(id, total);
+        Nota nota(idMeja, idPemesanan);
+        nota.hitungTotal(pelanggan);
         nota.generateDaftarPesanan(pelanggan);
         nota.printHeader(jumlahPelanggan);
         nota.printNota();
         std::cout << std::endl;
         for(int i = 0; i < pelanggan.size(); i++){
-            NotaIndividu newNotaIndividu(i + 1, pelanggan[i].total);
+            NotaIndividu newNotaIndividu(i + 1, idPemesanan);
+            newNotaIndividu.hitungTotal(pelanggan[i].pesanan);
             newNotaIndividu.generateJumlahPesanan(pelanggan[i].pesanan);
-            newNotaIndividu.printHeader(id, i + 1);
+            newNotaIndividu.printHeader(idMeja, i + 1);
             newNotaIndividu.printNota(pelanggan[i].pesanan);
         }
         system("PAUSE");
@@ -199,6 +233,20 @@ class Meja{
     
 };
 
+
+int randomizer() {
+    // Use a random device as the seed for the random number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // Define the distribution for 6-digit numbers
+    std::uniform_int_distribution<int> dist(100000, 999999);
+
+    // Generate a random number
+    int randomNumber = dist(gen);
+
+    return randomNumber;
+}
 
 
 int main(){
@@ -211,13 +259,26 @@ int main(){
     std::cin >> pilihan;
     switch(pilihan){
         case 1:
-        int idMeja;
-        int jumlahPelanggan;
-        std::cout << "Masukkan id meja: ";
-        std:: cin >> idMeja;
-        std::cout << "Masukkan jumlahPelanggan: ";
-        std::cin >> jumlahPelanggan;
-        Meja meja(idMeja, jumlahPelanggan, menu);
+        {
+            int idMeja;
+            int jumlahPelanggan;
+            std::cout << "Masukkan id meja: ";
+            std:: cin >> idMeja;
+            std::cout << "Masukkan jumlah pelanggan: ";
+            std::cin >> jumlahPelanggan;
+            Meja meja(idMeja, jumlahPelanggan, randomizer(), menu);
+            break;
+        }
+        case 2:
+        {
+            std::cout << "Program selesai" << std::endl;
+            break;
+        }
+        default:
+        {
+        std::cout << "Pilihan tidak tersedia" << std::endl;
+        break;
+        }
     }
 
     
